fd9fa8379f7993e591e7aae59edea205f37fe56e
Dries Bielen <dries.bielen10@gmail.com>
2024-02-26 20:48:51 +0100

    made exxxercise 13

--- Chapter7/Exercise13/MainWindow.xaml.cs
+++ Chapter7/Exercise13/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Exercise13
 {
@@ -7,6 +8,23 @@ namespace Exercise13
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var price = Convert.ToInt32(priceLabel.Text);
+            var btw = 0.0;
+
+            if (rateCheckBox.IsChecked == true)
+            {
+                btw = price * 0.06;
+            } else
+            {
+                btw = price * 0.21;
+            }
+
+            btwLabel.Text = $"{btw}";
+            totalTextBox.Text = $"{price + btw}";
         }
     }
 }
