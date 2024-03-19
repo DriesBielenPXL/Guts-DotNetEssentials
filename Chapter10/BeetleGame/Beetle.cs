db830327671210d6e87626f69ef3833b01269640
Dries Bielen <dries.bielen10@gmail.com>
2024-03-19 16:46:30 +0100

    BEETLE OEFENING

--- Chapter10/BeetleGame/Beetle.cs
+++ Chapter10/BeetleGame/Beetle.cs
@@ -8,7 +8,121 @@ namespace BeetleGame
 {
     public class Beetle
     {
-       
+        private int _x;
+        private int _y;
+        private int _size;
+        private Ellipse _ellipse;
+        private bool _isVisible = false;
+        private Canvas _canvas;
 
+        public Beetle(Canvas canvas, int x, int y, int size)
+        {
+            _x = x;
+            _y = y;
+            _canvas = canvas;
+            _size = size;
+            CreateEllipse();
+            _isVisible = false;
+            Up = true;
+            Right = true;
+        }
+
+        public double Speed { get; set; }
+        public int Size { get { return _size; } set { _size = value; } }
+        public bool Up { get; set; }
+        public bool Right { get; set; }
+        public int X { get { return _x; } set { _x = value; UpdateEllipse(); } }
+        public int Y { get { return _y; } set { _y = value; UpdateEllipse(); } }
+        public bool IsVisible
+        {
+            get { return _isVisible; }
+            set
+            {
+                _isVisible = value;
+                if (IsVisible)
+                {
+                    _ellipse.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    _ellipse.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        public double ComputeDistance(DateTime time1, DateTime time2)
+        {
+            TimeSpan time = time2 - time1;
+            int seconds = Convert.ToInt32(time.TotalSeconds);
+            return (double)(seconds * Speed) / 100;
+        }
+
+        private void CreateEllipse()
+        {
+            _ellipse = new Ellipse
+            {
+                Width = _size,
+                Height = _size,
+                Margin = new Thickness(_x - (_size / 2), _y - (_size / 2), 0, 0),
+                Stroke = new SolidColorBrush(Colors.Red),
+                Fill = new SolidColorBrush(Colors.Red)
+            };
+            _canvas.Children.Add(_ellipse);
+        }
+
+        private void UpdateEllipse()
+        {
+            _ellipse.Width = Size;
+            _ellipse.Height = Size;
+            _ellipse.Margin = new Thickness(_x - (_size / 2), _y - (_size / 2), 0, 0);
+        }
+
+        public void ChangePosition()
+        {
+            if (Speed == 0)
+            {
+                return;
+            }
+
+            if (Up)
+            {
+                Y -= 1;
+            }
+            else
+            {
+                Y += 1;
+            }
+
+            if (Right)
+            {
+                X += 1;
+            }
+            else
+            {
+                X -= 1;
+            }
+            ChangeDirection();
+        }
+
+        private void ChangeDirection()
+        {
+            if (X <= Size / 2)
+            {
+                Right = true;
+            }
+            else if (X >= _canvas.Width - (_size / 2))
+            {
+                Right = false;
+            }
+
+            if (Y <= Size / 2)
+            {
+                Up = false;
+            }
+            else if (Y >= _canvas.Height - (_size / 2))
+            {
+                Up = true;
+            }
+        }
     }
 }
--- Chapter10/BeetleGame/MainWindow.xaml.cs
+++ Chapter10/BeetleGame/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
 
@@ -11,10 +13,103 @@ namespace BeetleGame
     /// </summary>
     public partial class MainWindow : Window
     {
-      
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private Beetle _beetle;
+        private DateTime _startTime;
+
         public MainWindow()
         {
-            InitializeComponent();          
+            InitializeComponent();
+            _timer.Tick += _timer_Tick;
+
+            GenerateLocation(out int x, out int y);
+            _beetle = new Beetle(paperCanvas, x, y, (int)sizeSlider.Value);
+
+
+            _timer.Interval = TimeSpan.FromMilliseconds(200);
+
+            sizeSlider.ValueChanged += SizeSlider_ValueChanged;
+            speedSlider.ValueChanged += SpeedSlider_ValueChanged;
+        }
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            _beetle.ChangePosition();
+        }
+
+        private void GenerateLocation(out int x, out int y)
+        {
+            Random random = new Random();
+            x = random.Next(Convert.ToInt32(paperCanvas.Width));
+            y = random.Next(Convert.ToInt32(paperCanvas.Height));
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (startButton.Content.ToString() == "Start")
+            {
+                _timer.Start();
+                _beetle.IsVisible = true;
+                startButton.Content = "Stop";
+                _startTime = DateTime.Now;
+                speedSlider.IsEnabled = false;
+                sizeSlider.IsEnabled = false;
+                messageLabel.Content = "";
+            }
+            else
+            {
+                startButton.Content = "Start";
+                _timer.Stop();
+                messageLabel.Content = Convert.ToString($"total distance in meters: {_beetle.ComputeDistance(_startTime, DateTime.Now)}");
+                speedSlider.IsEnabled = true;
+                sizeSlider.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Name == "upButton")
+            {
+                _beetle.Up = true;
+            }
+            else if (button.Name == "downButton")
+            {
+                _beetle.Up = false;
+            }
+            else if (button.Name == "rightButton")
+            {
+                _beetle.Right = true;
+            }
+            else
+            {
+                _beetle.Right = false;
+            }
+        }
+
+        private void SizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_beetle != null)
+            {
+                _beetle.Size = (int)sizeSlider.Value;
+                sizeLabel.Content = sizeSlider.Value;
+                _timer.Interval = CalculateInterval();
+            }
+        }
+
+        private void SpeedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_beetle != null)
+            {
+                _beetle.Speed = speedSlider.Value;
+                speedLabel.Content = speedSlider.Value;
+                _timer.Interval = CalculateInterval();
+            }
+        }
+
+        private TimeSpan CalculateInterval()
+        {
+            double interval = 100 / speedSlider.Value * sizeSlider.Value / 10;
+            return TimeSpan.FromMilliseconds(interval);
         }
 
     }
